ba9ee2169730ea3c4a6efd0e039b8f06f6524205
Domagoj Veseli <dveseli555@gmail.com>
2024-03-21 18:06:40 +0100

    Izbacio suvišne DTO-ove zasad

--- TCGApp/Models/DTO.cs
+++ TCGApp/Models/DTO.cs
@@ -10,34 +10,4 @@ namespace TCGApp.Models
       string prezime, string email, string mjesto, string drzava);
 
 
-
-
-
-
-    public record PolaznikDTORead(int sifra, string ime, string prezime,
-        string email, string oib, string brojugovora);
-
-    public record PolaznikDTOInsertUpdate(string ime, string prezime,
-        string email, string oib, string brojugovora);
-
-
-
-
-    public record PredavacDTORead(int sifra, string ime, string prezime,
-        string email, string oib, string iban, string datoteka);
-
-    public record PredavacDTOInsertUpdate(string ime, string prezime,
-        string email, string oib, string iban);
-
-
-
-    public record GrupaDTORead(int sifra, string? naziv,
-        string? smjerNaziv, string? predavacImePrezime, int brojpolaznika, DateTime? datumpocetka, int? maksimalnopolaznika);
-    // ako se parametar zove kao svojstvo nekog tipa u toj klasi tada uzima punu putanju klase (npr. EdunovaAPP.Models.Predavac)
-
-
-
-    public record GrupaDTOInsertUpdate(string? naziv,
-        int? smjerSifra, int? predavacSifra, DateTime? datumpocetka, int? maksimalnopolaznika = 0);
-
 }
